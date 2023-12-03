fb221c2365d2d62f651a2bf89a6f88b8edd0a623
arcticronin <ronin.git@gmail.com>
2023-12-04 00:20:21 +0100

    day3

---
+++ day_03/main.hs
@@ -0,0 +1,74 @@
+import Data.Char (isDigit) -- isSymbol) --, digitToInt)
+import Data.List (isPrefixOf, nub)--, group)
+--import Distribution.Simple.Utils (xargs)
+-- nub for taking uniques in recyursion I took the first digit position twice
+
+sensitivePositions:: [Int] -> [Int]
+sensitivePositions x = y
+  where
+    y = nub $ filter (\y -> y >= 0) [(head x)-1 .. ((last x)+1)]
+
+getIntsAndPositions :: String -> [(Int, [Int])]
+getIntsAndPositions str = map (\(num, pos) -> (read num, pos)) parsedNumsAndPos
+  where
+    parsedNumsAndPos = map (\x -> (filter isDigit x, sensitivePositions $ findAllIndices (filter isDigit x) str)) intStrs
+    intStrs = filter (any isDigit) $ groupByAdjacentDigits str
+
+groupByAdjacentDigits :: String -> [String]
+groupByAdjacentDigits [] = []
+groupByAdjacentDigits (x:xs)
+  | isDigit x = let (digits, rest) = span isDigit (x:xs)
+                in digits : groupByAdjacentDigits rest
+  | otherwise = groupByAdjacentDigits xs
+
+findAllIndices :: Eq a => [a] -> [a] -> [Int]
+findAllIndices xs = go 0
+  where
+    go _ [] = []
+    go n ys
+      | xs `isPrefixOf` ys = n : map (+ n) [0 .. length xs - 1] ++ go (n + 1) (tail ys)
+      | otherwise = go (n + 1) (tail ys)
+
+getHotPositions::String -> Int -> [Int]
+getHotPositions [] _ = []
+getHotPositions (x:xs) n
+  | isaHot x = n: getHotPositions xs (n+1)
+  | otherwise = getHotPositions xs (n+1)
+
+part1 :: IO ()
+part1 = do
+  contents <- readFile "input.txt"  -- Read the file
+  let linesOfFile = lines contents  -- Split the content into lines
+      hotPositions = map (\line -> getHotPositions line 0) linesOfFile  -- Apply getHotPositions to each line
+      intsAndPositions = map getIntsAndPositions linesOfFile  -- Apply getIntsAndPositions to each line
+      hp = map fuse $ zip (map fuse $ zip hotPositions ((tail hotPositions)++ [[]])) ([[]] ++ hotPositions)
+      m = zipWith checkOverlap intsAndPositions hp
+      result = sum $ map sum m -- 304
+  --print $ length m
+  --mapM_ print m
+  --print $ sum $ map sum m
+  --mapM_ print $ map (filter (\x -> x < 305)) m
+  print $ sum $ map sum m
+
+fuse :: ([Int],[Int]) -> [Int]
+fuse (a,b) = c where
+  c = nub $ a++b
+
+checkOverlap :: [(Int, [Int])] -> [Int] -> [Int]
+checkOverlap tuples list =
+  [num | (num, positions) <- tuples, any (`elem` list) positions]
+
+isaHot:: Char -> Bool
+isaHot c
+  | c == '1' = False
+  | c == '2' = False
+  | c == '3' = False
+  | c == '4' = False
+  | c == '5' = False
+  | c == '6' = False
+  | c == '7' = False
+  | c == '8' = False
+  | c == '9' = False
+  | c == '0' = False
+  | c == '.' = False
+  | otherwise = True
