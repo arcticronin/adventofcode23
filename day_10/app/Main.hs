673661c095f39770778b9ab31b3f8ee9f042f1d7
arcticronin <ronin.git@gmail.com>
2023-12-11 23:49:01 +0100

    day10P2, using shoelace formula

--- day_10/app/Main.hs
+++ day_10/app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Data.Vector.Unboxed (Vector, (!), fromList, findIndex)
+import Data.List (maximumBy)
+import Data.Ord (comparing)
 
 data Matrix = Matrix (Vector Char) Int
 
@@ -28,16 +30,15 @@ indexMatrix (Matrix vec width) (row, col) = vec ! (row * width + col)
 
 getStartingIndex:: Matrix -> (Int, Int)
 getStartingIndex (Matrix m w) = transformIndex w index
-  where Just index = findIndex (\x -> x=='S') m
+  where Just index = findIndex (\x -> x=='S') m -- unsafe unwrap
 
--- get info on a road :: Matrix (starting coordinates) (current coordinates) Length
-roadToLength:: Matrix -> (Int, Int) -> (Int, Int) -> Int -> Int
-roadToLength m (_, _) (i,j) n
-  |  i < 0 || j < 0 = 0
-  |  indexMatrix m (i,j) == '.' = -2 -- ended on a dot
-  |  indexMatrix m (i,j) == 'S' = n
+roadToLength:: Matrix -> (Int, Int) -> (Int, Int) -> (Int, [(Int,Int)]) -> (Int, [(Int,Int)])
+roadToLength m (_, _) (i,j) (n, path)
+  |  i < 0 || j < 0 = (0, [])
+  |  indexMatrix m (i,j) == '.' = (-2, []) -- ended on a dot
+  |  indexMatrix m (i,j) == 'S' = (n, (i,j):path)
 
-roadToLength m (li, lj) (i, j) n
+roadToLength m (li, lj) (i, j) (n, path)
 
   -- comung from up
   | currSym == '|' && (li < i ) = roadToLength m (i,j) (i+1, j) newN -- i'm going down
@@ -68,27 +69,35 @@ roadToLength m (li, lj) (i, j) n
   | currSym == 'F' && (lj > j ) = roadToLength m (i,j) (i+1, j) newN -- i'm going down
   -- coming from down
   | currSym == 'F' && (li > i ) = roadToLength m (i,j) (i, j+1) newN -- i'm going rigth
-  | otherwise = -1 -- ended on a disconnected pipe
+  | otherwise = (-1, []) -- ended on a disconnected pipe
     where
       currSym = indexMatrix m (i, j)
-      newN = n + 1
+      newN = (n + 1, (i,j):path)
 
-explorePaths:: Matrix -> (Int, Int) -> [Int]
+explorePaths:: Matrix -> (Int, Int) -> [(Int, [(Int, Int)])]
 explorePaths matrix (i,j) =
   [n,e,s,w]
   where
-    n = roadToLength matrix (i,j) (i-1,j) 1 -- go down
-    e = roadToLength matrix (i,j) (i,j+1) 1 -- go down
-    s = roadToLength matrix (i,j) (i+1,j) 1 -- go down
-    w = roadToLength matrix (i,j) (i,j-1) 1 -- go down
+    n = roadToLength matrix (i,j) (i-1,j) (1,[(i,j)]) -- go down
+    e = roadToLength matrix (i,j) (i,j+1) (1,[(i,j)]) -- go down
+    s = roadToLength matrix (i,j) (i+1,j) (1,[(i,j)]) -- go down
+    w = roadToLength matrix (i,j) (i,j-1) (1,[(i,j)]) -- go down
+
+shoeLace :: [(Int, Int)] -> Int
+shoeLace [] = 0
+shoeLace [_] = 0
+shoeLace ((x1, y1) : (x2, y2) : xs) = (y1 + y2) * (x2 - x1) + shoeLace ((x2, y2) : xs)
+
+
+getInnerArea :: [(Int, Int)] -> Int
+getInnerArea points = (sl - lengthOutside + 3) `div` 2
+  where
+    sl = shoeLace points
+    lengthOutside = length points
 
 main :: IO ()
 main = do
     matrix <- readFileToMatrix "input.txt"
-    -- Closure s (Currying)
-    let mAt = indexMatrix matrix
-    let startingIndex = getStartingIndex matrix
-    let results = explorePaths matrix startingIndex
-    print startingIndex
-    print results
-    print $ maximum $ map (\x -> x `div` 2)  results
+    let (result, path) = maximumBy (comparing fst) (explorePaths matrix $ getStartingIndex matrix)
+    print result -- part 1
+    print $ getInnerArea path -- part 2
